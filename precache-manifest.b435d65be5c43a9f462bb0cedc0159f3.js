d2b9ec5e3f8f8c738deaffe34744aa3ccd624efb
Drapeto <drapetorres@gmail.com>
2020-07-21 15:43:07 -0700

    Updates

---
+++ precache-manifest.b435d65be5c43a9f462bb0cedc0159f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6944575bef0761c155ad9a86d270a986",
+    "url": "/expense-tracker-react/index.html"
+  },
+  {
+    "revision": "4b9eaf38d288a54f86aa",
+    "url": "/expense-tracker-react/static/css/main.ce28fbe8.chunk.css"
+  },
+  {
+    "revision": "a3886eb48eeb822dea54",
+    "url": "/expense-tracker-react/static/js/2.abb637fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker-react/static/js/2.abb637fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b9eaf38d288a54f86aa",
+    "url": "/expense-tracker-react/static/js/main.a783eddf.chunk.js"
+  },
+  {
+    "revision": "27d5a3916d5c0aa966b2",
+    "url": "/expense-tracker-react/static/js/runtime-main.69aaf8cd.js"
+  }
+]);
